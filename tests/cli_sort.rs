6149f507288b3048cd31a7f51212a6eb452235e3
canardleteer <eris.has.a.dad+github@gmail.com>
2025-02-23 18:17:40 -0800

    feat: proptest integration
    This branch was abandoned ~8 months ago, and is now being brough
    forward. Property Tests can certainly be made more robust, but that's
    easier to do once upstream on an as needed basis.

--- tests/cli_sort.rs
+++ tests/cli_sort.rs
@@ -13,6 +13,9 @@
 //! See the License for the specific language governing permissions and
 //! limitations under the License.
 use assert_cmd::Command;
+use proptest::prelude::*;
+use proptest_semver::*;
+use semver::{Version, VersionReq};
 
 mod common;
 use common::subcommands::*;
@@ -151,4 +154,67 @@ fn cli_sort_basic_cases() {
     assert
         .append_context(COMMAND_SORT, "2 items, --lexical-sorting, --flatten")
         .success();
+}
+
+const SORT_TEST_VERSION_COUNT_SMALL: usize = 32;
+const SORT_TEST_VERSION_COUNT_LARGE: usize = 128;
+
+fn sort_test_generic(
+    lexical_sorting: bool,
+    reverse: bool,
+    flatten: bool,
+    filter: Option<VersionReq>,
+    versions: Vec<Version>,
+) {
+    let mut args = vec![COMMAND_SORT.to_string()];
+    if lexical_sorting {
+        args.push("--lexical-sorting".to_string());
+    }
+    if reverse {
+        args.push("--reverse".to_string());
+    }
+    if flatten {
+        args.push("--flatten".to_string());
+    }
+
+    if let Some(filter) = filter {
+        args.push("--filter".to_string());
+        args.push(format!("{}", filter));
+    }
+
+    args.append(
+        &mut versions
+            .iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>(),
+    );
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let assert = cmd.args(&args).assert();
+    assert.append_context(COMMAND_SORT, "prop test").success();
+}
+
+proptest! {
+    #![proptest_config(ProptestConfig {
+        // Setting both fork and timeout is redundant since timeout implies
+        // fork, but both are shown for clarity.
+        fork: true,
+        // timeout: 10000,
+        cases: 256,
+        .. ProptestConfig::default()
+    })]
+    // Using some large number of filters is unlikely to provide us with half the
+    // test cases,
+    #[test]
+    fn sort_test_small(lexical_sorting: bool, reverse: bool, flatten: bool, filter in arb_optional_version_req(0.5, 2), versions in arb_vec_versions(SORT_TEST_VERSION_COUNT_SMALL)) {
+        sort_test_generic(lexical_sorting, reverse, flatten, filter, versions);
+    }
+
+    // Since the filters are incredibly complex from the framework, the odds of
+    // not making mutually exclusive comparators is small. We're just testing
+    // huge input here.
+    #[test]
+    fn sort_test_large(lexical_sorting: bool, reverse: bool, flatten: bool, filter in arb_optional_version_req(0.5, 2), versions in arb_vec_versions(SORT_TEST_VERSION_COUNT_LARGE)) {
+        sort_test_generic(lexical_sorting, reverse, flatten, filter, versions);
+    }
 }
